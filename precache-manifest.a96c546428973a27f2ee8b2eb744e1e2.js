fd5df18dbc5973b1d57686313b4e7a4956bb34c0
kwonh11 <kwonh11@gmail.com>
2020-07-05 02:43:14 +0900

    Updates

---
+++ precache-manifest.a96c546428973a27f2ee8b2eb744e1e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a34e3011e5a4896f8f71",
+    "url": "/clone-movie/static/css/main.1c467b8d.chunk.css"
+  },
+  {
+    "revision": "a34e3011e5a4896f8f71",
+    "url": "/clone-movie/static/js/main.7faee56e.chunk.js"
+  },
+  {
+    "revision": "50d84979d873d9193984",
+    "url": "/clone-movie/static/js/runtime~main.86e39789.js"
+  },
+  {
+    "revision": "a0cfa739f703a87c0bfe",
+    "url": "/clone-movie/static/js/2.581849f7.chunk.js"
+  },
+  {
+    "revision": "6f7fe67293db003ec97501302e3e3552",
+    "url": "/clone-movie/index.html"
+  }
+];
